d003b7ff41516854e3ea7134ee397496f01e4ef3
KH <knhg2013@gmail.com>
2020-01-03 15:15:55 -0500

    Complete the testing string generation, also the user can select whether to use generated testing string

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <cstring>
+#include <ctime>
 
 using namespace std;
 
 void display(char *ary, int size) {
 	for (int i = 0; i < size; i++){
-		cout << ary[i] << "  ";
+		cout << ary[i] << " ";
 	}
 	cout << endl;
 }
@@ -78,26 +80,61 @@ void countFastProcess(char ary[], int size){
 	displayPair(&result, result.size());
 }
 
+string generateTestingStr(){
+	//generate 10 different characters from a~z, in sequence;
+	const int MAX = 26, MAXREPEAT = 91;
+	char txtChar;
+	int typeLen = 10, tstPt;
+	char alphabet[MAX] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+		'h', 'i', 'j', 'k', 'l', 'm', 'n',
+		'o', 'p', 'q', 'r', 's', 't', 'u',
+		'v', 'w', 'x', 'y', 'z' };
+	string res = "";
+	srand(time(NULL));  //  using the time seed from srand explanation 
+	for (int i = 0; i < typeLen; i++){
+		res = res + alphabet[rand() % MAX];
+	}
+	sort(res.begin(), res.end()); // generate sorted string with 10 chars randomly
+	for (int i = res.size() - 1; i >= 0; i--){
+		tstPt = rand() % MAXREPEAT + 9; // generate random number 9~99
+		res.insert( i, string(tstPt, res.at(i)) ); // repeat one char 10~100 times randomly
+	}
+	return res; // generate a sorted string with 10 (typeLen) chars, total length 100~1000 characters
+}
+
 int main() {
-	int size;
-	char ipt[] = "abbbbbdhhhp";
-	cout << "Please enter the array. \n";
+	int size, iptVal;
+	string testStr = generateTestingStr();
+
+	cout << "\nPlease make the selection below: \n";
+	cout << "\nEnter 0 to use the quesion provided string. \nEnter anything else will use a random testing: \n";
+	char *ipt;
+	cin >> iptVal;
+	if (iptVal == 0){
+		ipt = "abbbbbdhhhp";
+	}
+	else{
+		
+		ipt = &testStr[0];	
+	}
+
 	size = strlen(ipt);
 	cout << endl;
 
-	cout << "The initial array is: \n";
+	cout << "The initial array for counting is: \n";
 	display(ipt, size);
 	cout << endl;
 
-	cout << "\nThe count reult is below, value -- frequency :";
+	cout << "\nThe regular O(N) count reult is below , value -- frequency :";
 	countProcess(ipt, size);
 	cout << endl;
 
-	cout << "\nThe fast count reult is below, value -- frequency :";
+	cout << "\nThe fast O(logN) count reult is below, value -- frequency :";
 	countFastProcess(ipt, size);
 	cout << endl;
 
-	cout << "\nPress any key to exit this console application.";
+	cout << "\nPress enter key to exit this console application.";
 	cin.ignore();
+	cin.get();
 	return 1; // An non-zero value indicates the app succeeds.
 }
